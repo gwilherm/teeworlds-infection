3d310eb42dbc08189334437d1b2da335c9fc2f99
Gwilherm D <d.gwilherm@yahoo.com>
2016-03-02 20:10:47 +0100

    Double zombie explosion force

--- src/game/server/gamecontext.h
+++ src/game/server/gamecontext.h
@@ -125,7 +125,7 @@ public:
 
 	// helper functions
 	void CreateDamageInd(vec2 Pos, float AngleMod, int Amount);
-	void CreateExplosion(vec2 Pos, int Owner, int Weapon, bool NoDamage);
+	void CreateExplosion(vec2 Pos, int Owner, int Weapon, bool NoDamage, int PowerFactor);
 	void CreateHammerHit(vec2 Pos);
 	void CreatePlayerSpawn(vec2 Pos);
 	void CreateDeath(vec2 Pos, int Who);
